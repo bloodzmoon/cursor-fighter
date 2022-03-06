864afd2a27160ee697a02e8278d7a0b2d6dfe699
bloodzmoon <thanyathon.dev@gmail.com>
2022-03-06 18:57:08 +0700

    add check mouse in screen

--- frontend/src/components/Player.svelte
+++ frontend/src/components/Player.svelte
@@ -6,34 +6,51 @@
   import cursor01 from '../assets/cursor01.png'
 
   let isClicked = false
-  let mX = 0,
-    mY = 0
+  let isInScreen = false
+  let mX = 0
+  let mY = 0
 
   onMount(() => {
     const cursor = PIXI.Sprite.from(cursor01)
     $appContext.stage.addChild(cursor)
 
-    $appContext.stage.on('mousedown', (e) => {
+    $appContext.stage.on('mousedown', (e: PIXI.InteractionEvent) => {
       isClicked = true
 
       let pos = e.data.global
       cursor.x = pos.x
       cursor.y = pos.y
     })
-    $appContext.stage.on('mouseup', (e) => {
+    $appContext.stage.on('mouseup', () => {
       isClicked = false
     })
-    $appContext.stage.on('mousemove', (e: PIXI.InteractionEvent) => {
+    $appContext.stage.on('mouseover', () => {
+      isInScreen = true
+    })
+    $appContext.stage.on('mouseout', () => {
+      isInScreen = false
+    })
+    $appContext.stage.on('pointermove', (e: PIXI.InteractionEvent) => {
+      if (!isInScreen) return
+
       let pos = e.data.global
       mX = pos.x
       mY = pos.y
 
-      if (isClicked) {
-        cursor.x = pos.x
-        cursor.y = pos.y
-      }
+      if (!isClicked) return
+      cursor.x = pos.x
+      cursor.y = pos.y
     })
   })
 </script>
 
-<p style="color: white;">isClick: {isClicked} / X: {mX} / Y: {mY}</p>
+<p>isInScreen: {isInScreen} / isClick: {isClicked} / X: {mX} / Y: {mY}</p>
+
+<style>
+  p {
+    color: white;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  }
+</style>
